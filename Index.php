9143edd0b16827ca9589894376bc20f046bce3ca
jpcr007l <jpcr007l@gmail.com>
2026-09-17 17:18:10 -0600

    Trabajamos con php y con estructura basica de html

---
+++ Index.php
@@ -0,0 +1,41 @@
+<?php
+
+echo "Hola Mundo";
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        /* texto */
+        h1{
+            color: blue;
+        }   
+
+        div {
+            /* fondo */
+             background : pink;
+        }
+</style>
+
+</head>
+<body>
+  <h1>hola crayola</h1>
+  <h2>hola crayola</h2>
+  <h3>hola crayola</h3>
+  <h4>hola crayola</h4>
+  <h5>hola crayola</h5>
+  <h6>hola crayola</h6>
+
+  <div>
+    <h2>Titulo del parrafo</h2>
+
+    <p>Este es un parrafo</p>
+  </div>
+
+</body>
+</html>
